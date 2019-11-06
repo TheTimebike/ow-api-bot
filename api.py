a7506e0f6518ae210eff6d0be4e8bc9763ad7f23
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-11-06 19:40:13 +0000

    update

---
+++ api.py
@@ -0,0 +1,67 @@
+import json, urllib.parse, http.cookies
+http.cookies._is_legal_key = lambda _: True
+import requests as _requests
+from os import path, makedirs
+
+BASE_ROUTE = "https://owapi.net/api/v3"
+STATS_ROUTE = "https://owapi.net/api/v3/u/{0}/stats"
+HEROES_ROUTE = "https://owapi.net/api/v3/u/{0}/heroes"
+ACHIEVEMENT_ROUTE = "https://owapi.net/api/v3/u/{0}/achievements"
+
+class Config:
+    def __init__(self, directory, server_id):
+        self.directory = "./{0}/".format(directory)
+        self.filepath = "./{0}/{1}.json".format(directory, server_id)
+        self.make()
+    
+    def make(self):
+        if not path.exists(self.directory):
+            makedirs(self.directory)
+        if not path.isfile(self.filepath):
+            with open(self.filepath, "w+") as out:
+                json.dump({
+                    "region": "us",
+                    "role": None,
+                    "bronze_id": None,
+                    "silver_id": None,
+                    "gold_id": None,
+                    "platinum_id": None,
+                    "diamond_id": None,
+                    "master_id": None,
+                    "grandmaster_id": None,
+                    "members": {}
+                }, out, indent=4)
+
+    def load(self):
+        with open(self.filepath, "r") as out:
+            config = json.load(out)
+        return config
+
+    def save(self, new_config):
+        with open(self.filepath, "w+") as out:
+            json.dump(new_config, out, indent=4)
+
+    def update(self, key, attr):
+        conf = self.load()
+        if key in conf.keys():
+            conf[key] = attr
+        self.save(conf)
+
+    def get_conversion_table(self, table):
+        _index = _requests.get("https://raw.githubusercontent.com/TheTimebike/ow-api-bot/master/conversion_tables/index.json")
+        _data_url = _index.json()[table]
+        _data = _requests.get(_data_url)
+        return _data.json()      
+
+class Api:
+    def __init__(self, api_token=None):
+        self.api_token = api_token
+        self.headers = {
+            "X-API-Key": self.api_token,
+            "User-agent": "OWMN Bot"
+        }
+
+    def get(self, request):
+        self._requestData = _requests.get(urllib.parse.quote(request, safe=':/?&=,.'), headers=self.headers).json()
+        #print(json.dumps(self._requestData, indent=4))
+        return self._requestData
---
+++ main.py
@@ -0,0 +1,128 @@
+import api, discord
+from discord.utils import get as get_role
+import auth
+client = discord.Client()
+
+class StatServer:
+    def __init__(self, server):
+        self.server = server
+        self.config = api.Config("ow-configs", self.server.id)
+
+#api.Api().get(api.STATS_ROUTE.format("TheTimebike-2349"))
+#serv = StatServer("123")
+#serv.config.update("role", "support")
+
+@client.event
+async def on_server_join(server):
+    statserv = StatServer(server)
+
+@client.event
+async def on_message(message):
+    serv = StatServer(message.author.server)
+    config = serv.config.load()
+
+    if message.content.lower().startswith(".register "):
+        username = message.content[len(".register "):]
+        new_members = serv.config.load()["members"]
+        new_members[message.author.id] = username.replace("#", "-")
+        serv.config.update("members", new_members)
+        await client.send_message(message.channel, "Config Updated! Your account is now bound to **{0}**".format(username))
+
+    elif message.content.lower().startswith(".config region "):
+        new_region = message.content.lower()[len(".config region "):]
+        if new_region not in ["kr", "eu", "us"]:
+            await client.send_message(message.channel, "Please select a region from the following: \n**KR**, **EU** or **US**.")
+            return
+        serv.config.update("region", new_region)
+        await client.send_message(message.channel, "Config Updated! Your new region is **{0}**.".format(new_region.upper()))
+
+
+    elif message.content.lower().startswith(".config role "):
+        new_role = message.content.lower()[len(".config role "):]
+        if new_role not in ["support", "tank", "damage"]:
+            await client.send_message(message.channel, "Please select a role from the following: \n**SUPPORT**, **TANK** or **DAMAGE**.")
+            return
+        serv.config.update("role", new_role)
+        await client.send_message(message.channel, "Config Updated! Your new role is **{0}**.".format(new_role.title()))
+
+    elif message.content.lower().startswith(".config rank roles "):
+        bronze_id = message.role_mentions[0].id
+        serv.config.update("bronze_id", bronze_id)
+
+        silver_id = message.role_mentions[1].id
+        serv.config.update("silver_id", silver_id)
+
+        gold_id = message.role_mentions[2].id
+        serv.config.update("gold_id", gold_id)
+
+        platinum_id = message.role_mentions[3].id
+        serv.config.update("platinum_id", platinum_id)
+        
+        diamond_id = message.role_mentions[4].id
+        serv.config.update("diamond_id", diamond_id)
+
+        master_id = message.role_mentions[5].id
+        serv.config.update("master_id", master_id)
+
+        grandmaster_id = message.role_mentions[6].id
+        serv.config.update("grandmaster_id", grandmaster_id)
+
+        await client.send_message(message.channel, "Config Updated! Those roles have been set for the competitive rank roles.")
+
+    elif message.content.lower().startswith(".config bronze "):
+        role_id = message.role_mentions[0].id
+        serv.config.update("bronze_id", role_id)
+        await client.send_message(message.channel, "Config Updated! Your new bronze role is **{0}**".format(message.role_mentions[0].mention))
+
+    elif message.content.lower().startswith(".config silver "):
+        role_id = message.role_mentions[0].id
+        serv.config.update("silver_id", role_id)
+        await client.send_message(message.channel, "Config Updated! Your new bronze role is **{0}**".format(message.role_mentions[0].mention))
+
+    elif message.content.lower().startswith(".config gold "):
+        role_id = message.role_mentions[0].id
+        serv.config.update("gold_id", role_id)
+        await client.send_message(message.channel, "Config Updated! Your new silver role is **{0}**".format(message.role_mentions[0].mention))
+
+    elif message.content.lower().startswith(".config platinum "):
+        role_id = message.role_mentions[0].id
+        serv.config.update("platinum_id", role_id)
+        await client.send_message(message.channel, "Config Updated! Your new platinum role is **{0}**".format(message.role_mentions[0].mention))
+
+    elif message.content.lower().startswith(".config diamond "):
+        role_id = message.role_mentions[0].id
+        serv.config.update("diamond_id", role_id)
+        await client.send_message(message.channel, "Config Updated! Your new diamond role is **{0}**".format(message.role_mentions[0].mention))
+
+    elif message.content.lower().startswith(".config master "):
+        role_id = message.role_mentions[0].id
+        serv.config.update("master_id", role_id)
+        await client.send_message(message.channel, "Config Updated! Your new master role is **{0}**".format(message.role_mentions[0].mention))
+
+    elif message.content.lower().startswith(".config grandmaster "):
+        role_id = message.role_mentions[0].id
+        serv.config.update("grandmaster_id", role_id)
+        await client.send_message(message.channel, "Config Updated! Your new grandmaster role is **{0}**".format(message.role_mentions[0].mention))
+
+    elif message.content.lower().startswith(".config"):
+        config = serv.config.load()
+        embed=discord.Embed()
+        embed=discord.Embed(title="Ow Bot Config", description="Configuration for the OW Role Bot")
+        embed.add_field(name="Region\nRole", value="** **", inline=True)
+        embed.add_field(name="{0}\n{1}".format(config["region"].upper(), config["role"].title()), value="** **", inline=True)
+        embed.set_footer(text="Made by u/TheTimebike")
+        await client.send_message(message.channel, embed=embed)
+
+    elif config["members"].get(message.author.id, None) != None:
+        stats = api.Api().get(api.STATS_ROUTE.format(config["members"][message.author.id]))
+        rank = stats[config["region"]]["stats"]["competitive"]["overall_stats"]["{0}_tier".format(config["role"])]
+        to_remove, ranks = [], ["bronze", "silver", "gold", "platinum", "diamond", "master", "grandmaster"]
+        for rank_name in ranks:
+            if rank_name.lower() != rank.lower():
+                to_remove.append(get_role(message.author.server.roles, id=config["{0}_id".format(rank_name)]))
+
+        for _ in to_remove:
+            await client.remove_roles(message.author, to_remove[0], to_remove[1], to_remove[2], to_remove[3], to_remove[4], to_remove[5])
+        await client.add_roles(message.author, get_role(message.author.server.roles, id=config["{0}_id".format(rank)]))
+
+client.run(auth.token)
